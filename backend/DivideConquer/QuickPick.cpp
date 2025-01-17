9e8418a4c3ff0119945dcd05f5e7eec0531c4320
rohungowda <rohungowda@gmail.com>
2025-01-17 15:52:51 -0500

    2ndIterationQuickPick

--- backend/DivideConquer/QuickPick.cpp
+++ backend/DivideConquer/QuickPick.cpp
@@ -4,74 +4,147 @@
 #include <queue>
 #include <iostream>
 #include <algorithm>
+#include <random>
+#include <chrono>
 
 
 // maxHeap automatically is one
 // heapq tokes n log(k) to find the element
 // QuickPick should take O(n)
 
-int QuickPick(std::vector<int> S, int k){
-    if (k <= 5){
-         std::sort(S.begin(), S.end());
-        
-        return S[k];
-    }else{
-        // divide S into len(s)/5 groupsof 5 + remainder
-        int groups = S.size() / 5;
-        int remainder = S.size() % 5;
-        std::vector<int>::iterator start = S.begin();
-        std::vector<int>::iterator end = (S.begin() + 5);
+// [l, r)
+int QuickPick(std::vector<int>S, int k, int l, int r){
+        if (k <= 5){
+            std::priority_queue<int> pq;
+            for(int i = l; i < r; i++){
+                pq.push(S[i]);
+
+                if (pq.size() > k){
+                    pq.pop();
+                }
+            }
+
+            return pq.top();
+        }else{
+
+        int groups = (r-l) / 5;
+        int remainder = (r-l) % 5;
 
         std::vector<int> medians;
 
-        for(int i= 0; i < groups; i++){
+        for(int i= 0; i < groups; i+=1){
+            int start = (l + (i*5));
+            std::sort(S.begin() + start, S.begin() + start + 5);
+            int median = (start + (start + 4)) / 2;
+            medians.push_back(S[median]);
 
-            std::sort(start,end);
-            medians.push_back(*(start + 2));
-
-            start += 5;
-            if (i != (groups - 1)){
-                end += 5;
-            }
         }
 
         if (remainder){
-            end = start + remainder;
-            std::sort(start,end);
-
-            int k = remainder / 2;
-            medians.push_back(*(start + k));
+            int start = (r - remainder);
+            std::sort(S.begin() + start, S.begin() + r);
+            int median = (start + (r-1)) / 2;
+            medians.push_back(S[median]);
         }
 
-        
-        for(auto element : medians){
-            std::cout << element <<  " ";
+        int e = QuickPick(medians, (medians.size() / 2) + 1, 0, medians.size());
+
+        int pointerL = l;
+        int pointerM = l;
+        int pointerH = r - 1;
+
+        // Dutch Flag Partition
+        while(pointerM <= pointerH){
+            if (S[pointerM] == e){
+                pointerM += 1;
+            } else if(S[pointerM] < e){
+                std::swap(S[pointerM], S[pointerL]);
+                pointerM += 1;
+                pointerL += 1;
+
+            }else{
+                std::swap(S[pointerM], S[pointerH]);
+                pointerH -= 1;
+            }
         }
-        std::cout << std::endl;
 
-        for(auto element : S){
-            std::cout << element <<  " ";
+
+        int rank = (pointerL - l) + 1;
+
+        if (k >= rank && k < ((pointerM-l)+1)){
+            return e;
         }
-        std::cout << std::endl;
 
-
-        int e = QuickPick(medians, (medians.size() / 2));
-        return e;
+        if (k < rank){
+            return QuickPick(S, k, l, pointerL);
+        }else{
+            return QuickPick(S, k - (pointerM - l), pointerM, r);
+        }
 
     }
-
-    return 0;
 }
 
 int main(){
 
-    std::vector<int> test = {2,5,2,4,5,1,7,3,3,3,5,2,9};
-    std::cout << QuickPick(test,8) << std::endl;
+    std::vector<int> test;
+    std::random_device rd;
+
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(-21785, 21785);
+
+    for (int i = 0; i < 50000; ++i) {
+        test.push_back(distr(gen));
+    }
+    
+    std::uniform_int_distribution<> dist(1, test.size());
+    std::vector<int> saved = test;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::sort(saved.begin(), saved.end());
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> elapsed = end - start;
+    std::cout << "Time to Beat: Sort: " << elapsed.count() << " ms\n" << std::endl;
+
+    for(int i = 0; i < 3; i++){
+
+        int random = dist(gen);
+        std::cout << "Pick the " << random << "th smallest element" << std::endl;
+
+        auto start = std::chrono::high_resolution_clock::now();
+        int element = QuickPick(test, random, 0 , test.size());
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        std::cout << "Elapsed time for QuickPick: " << elapsed.count() << " ms\n";
+
+
+        if(saved[(random-1)] != element){
+            for(auto element : test){
+            std::cout << element <<  " ";
+            }
+            std::cout << std::endl;
+            std::cout << "Test FAILED, expected: " << saved[random] << " got " << element << std::endl;
+            return 1;
+        }
+
+        std::cout << "Sort: " << saved[(random-1)] << " / QuickPick: " << element << std::endl;
+
+        std::cout << "--------------------------------" << std::endl;
+    }
+
+
 
     return 0;
 }
 
 /*
+
+
+
+    std::cout << std::endl;
+
+
         for(auto element : medians){
             std::cout << element <<  " ";
         }
